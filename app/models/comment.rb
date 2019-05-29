af0c2784e1d40af6408f78981756f67980fcf717
zenott <mblevel@gmail.com>
2019-05-29 19:13:54 +0200

    Add validations to Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  validates :comment_text, presence: true, length: { in: 2..500 }
+  validates :user, presence: true
+  validates :post, presence: true
 end
