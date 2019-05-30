f90ec1cb4612b1dd4c1ed816f5d92362f66e8973
zenott <mblevel@gmail.com>
2019-05-30 12:23:30 +0200

    Remove password attribute from User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
 
   validates :username, presence: true, length: { minimum: 2 }, uniqueness: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password_digest, presence: true, length: { minimum: 10 }
 end
